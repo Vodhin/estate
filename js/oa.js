620d266a9a6ad07763a0fa7d8cfd158101181ad3
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-03-07 12:43:23 -0500

    Add files via upload

--- js/oa.js
+++ js/oa.js
@@ -10,14 +10,34 @@ var vreBasePath = vrePath.replace(vrePage,'');
   
   function estOAPrep(){
     console.log('OA Prep');
+    
+    if($('#estJSpth').hasClass('estOA')){}
+    
+    $('.estOABlock').each(function(i,blk){
+      var h3 = $(blk).find('h3');
+      var dv1 = $(blk).find('div.estOATabCont');
+      
+      $(h3).on({
+        click : function(e){
+          e.stopPropagation();
+          console.log(dv1);
+          if($(dv1).is(':visible')){$('.estOABlock').removeClass('expand');}
+          else{
+            $('.estOABlock').removeClass('expand').promise().done(function(){$(blk).addClass('expand')});
+            }
+          }
+        });
+      });
+    
+        
+    
+    
     }
   
   
   
   $(document).ready(function(){
-    if($('#estJSpth').hasClass('estOA')){
-      estOAPrep();
-      }
+    estOAPrep();
     });
 
 })(jQuery);
