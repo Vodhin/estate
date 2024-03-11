7a18b5b0d018d61cd83afd11ef76b48d74f8acbe
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-03-11 12:27:41 -0400

    Add files via upload

--- js/oa.js
+++ js/oa.js
@@ -5,31 +5,89 @@ var vrePath = window.location.pathname;
 var vrePathPts = vrePath.split('/');
 var vrePage = vrePathPts.pop();
 var vreBasePath = vrePath.replace(vrePage,'');
+var vreFeud = vreBasePath+'js/adm/feud.php';
+
+
+function estAlertLog(msg){
+  alert(msg);
+  console.log(msg);
+  }
+
 
 (function ($) {
   
+  function estProcDefDta(newDefs=null){
+    var defs = $('body').data('defs');
+    if(newDefs !== null){
+      defs.tbls = newDefs;
+      $('body').data('defs',defs);
+      }
+    
+    $.each(defs.tbls, function(tbl,tDta){
+      if(typeof tDta.form !== 'undefined'){
+        $.each(tDta.form, function(fname,fdta){
+          if(fdta.str == 'int' || fdta.type == 'idx'){
+            $(defs.tbls[tbl].dta).each(function(gi,gEle){
+              defs.tbls[tbl].dta[gi][fname] = Number(defs.tbls[tbl].dta[gi][fname]);
+              });
+            }
+          });
+        }
+      });
+    }
+  
+  
+  
+  
+  
+  function estPrepPropHrs(){
+    $('.estPrefCalActDay').each(function(i,btn){
+      if(!$(btn).hasClass('eleBound')){
+        $(btn).addClass('eleBound');
+        $(btn).on({
+          click : function(e){
+            e.preventDefault()
+            fld = $('input[name="'+$(this).data('for')+'"]');
+            if($(fld).val() == 1){
+              $(this).removeClass('btn-primary').addClass('btn-default');
+              $(fld).val(Number(0));
+              $(btn).closest('td').find('input[type="time"]').prop('disabled',true);
+              }
+            else{
+              $(this).removeClass('btn-default').addClass('btn-primary');
+              $(fld).val(1);
+              $(btn).closest('td').find('input[type="time"]').prop('disabled',false).removeProp('disabled');
+              }
+            }
+          });
+        }
+      });
+    }
+  
+  
+  
+  
+  
   function estOAPrep(){
     console.log('OA Prep');
+    estProcDefDta();
     
-    if($('#estJSpth').hasClass('estOA')){}
     
-    $('.estOABlock').each(function(i,blk){
-      var h3 = $(blk).find('h3');
-      var dv1 = $(blk).find('div.estOATabCont');
-      
-      $(h3).on({
+    $('.estOABlock').each(function(i,ele){
+      $(ele).find('h3').on({
         click : function(e){
           e.stopPropagation();
-          console.log(dv1);
-          if($(dv1).is(':visible')){$('.estOABlock').removeClass('expand');}
-          else{
-            $('.estOABlock').removeClass('expand').promise().done(function(){$(blk).addClass('expand')});
-            }
+          if($(ele).find('div.estOATabCont').is(':visible')){$(ele).removeClass('expand');}
+          else{$(ele).addClass('expand');}
           }
         });
       });
     
-        
+    $('.admin-ui-help-tip').each(function(i,ele){
+      
+      });
+    
+    estPrepPropHrs();
     
     
     }
@@ -37,7 +95,29 @@ var vreBasePath = vrePath.replace(vrePage,'');
   
   
   $(document).ready(function(){
-    estOAPrep();
+    $.ajax({
+      url: vreFeud+'?0||0',
+      type:'get',
+      data:{'fetch':2,'propid':Number($('input[name="prop_idx"]').val()),'rt':'js','tbl':''},
+      dataType:'json',
+      cache:false,
+      processData:true,
+      success: function(ret, textStatus, jqXHR){
+        console.log(ret);
+        if(typeof ret !== 'undefined' && ret !== null){
+          if(typeof ret.error !== 'undefined'){estAlertLog(ret.error);}
+          else{
+            $('body').data('defs',ret);
+            estOAPrep();
+            }
+          }
+        else{estAlertLog(jqXHR.responseText);}
+        },
+      error: function(jqXHR, textStatus, errorThrown){
+        console.log('ERRORS: '+textStatus+' '+errorThrown);
+        estAlertLog(jqXHR.responseText);
+        }
+      });
     });
 
 })(jQuery);
