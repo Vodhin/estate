6c03891df3a7679c46a1e1d464949348ee87310a
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-05-05 13:36:14 -0400

    Contact Form Added
    Preliminary Contact Form coding has been added to allow visitors to directly email and PM the seller or agent of a property.

--- js/listing.js
+++ js/listing.js
@@ -4,8 +4,7 @@ var vrePath = window.location.pathname;
 var vrePathPts = vrePath.split('/');
 var vrePage = vrePathPts.pop();
 var vreBasePath = vrePath.replace(vrePage,'');
-
-
+var vreFeud = vreBasePath+'ui/msg.php';
 
 function searchToObject() {
   var pairs = window.location.search.substring(1).split('.'), obj = {}, pair, i;
@@ -316,21 +315,77 @@ function htmlDecode(mode,xVal,dec=0){
   
   function estMsgSnd(){
     $('.estChkMsgRem').each(function(i,ele){
-      if(this.value !== 'istatrap@icloud.com'){
+      if(this.value !== 'itsatrap@'+$(this).data('domn')){
         $('#estMsgFormTabl').parent().remove();
         return;
         }
       }).promise().done(function(){
-          $('<button></button>',{'id':'estMsgSend2','class':'btn btn-primary'}).html($('#estMsgSend1').data('t1')+'<div>'+$('#estMsgSend1').data('t2')+'</div>').on({
+        var txts = $('#estMsgFormDiv').data('txts');
+        $('<button></button>',{'id':'estMsgSend2','class':'btn btn-primary'}).html(txts.send1).on({
             click : function(e){
               e.preventDefault();
-              //gather form elements
-              $('#estMsgFormTabl').parent().remove();
-              alert('Function not available yet');
+              var tdta = {};
+              tdta['msg_text'] = $('textarea[name="msg_text"]').val();
+              tdta['msg_mode'] = Number($('select[name="msg_mode"]').find('option:selected').val());
+              //tdta['msg_from_cc'] = ($('#msg-from-cc').is(':checked') ? 1 : 0);
+              $('#estMsgFormTB').find('input').each(function(i,ele){
+                tdta[$(ele).prop('name')] = $(ele).val();
+                }).promise().done(function(){
+                  //console.log(tdta);
+                  $.ajax({
+                    url: vreFeud,
+                    type:'post',
+                    data:{'sndMsg':'js','tdta':tdta},
+                    dataType:'json',
+                    cache:false,
+                    processData:true,
+                    success: function(ret, textStatus, jqXHR){
+                      console.log(ret);
+                      var msg = '';
+                      if(typeof ret.error !== 'undefined'){
+                        $(ret.error).each(function(ei,err){
+                          msg += (msg.length > 1 ? '.<br /><br />' : '')+htmlDecode(0,err);
+                          }).promise().done(function(){
+                            alert(msg);
+                            });
+                        }
+                      else{
+                        if(typeof ret.msg == 'undefined'){alert(htmlDecode(0,txts.txt0));}
+                        else{
+                          msg = htmlDecode(0,'<p>'+txts.txt6+' '+ret.msg.msg_to_name+'. '+txts.txt7+'</p>');
+                          if(Number(ret.msg.msg_email) == 1){
+                            msg += htmlDecode(0,'<p>'+txts.txt1+' '+ret.msg.msg_to_name+'.</p>');
+                            
+                            if(Number(ret.msg.msg_from_cc) == 1){
+                              msg += htmlDecode(0,'<p>'+txts.txt4+' '+ret.msg.msg_from_addr+' '+txts.txt5+'.</p>');
+                              }
+                            }
+                          else{msg += htmlDecode(0,'<p>'+txts.txt0+'.</p>');}
+                          
+                          if(Number(ret.msg.msg_pm) > 0){
+                            msg += htmlDecode(0,'<p>'+ret.msg.msg_to_name+' '+txts.txt3+'.</p>');
+                            }
+                          $('#estMsgFormTabl').remove();
+                          }
+                        
+                        $('<div></div>',{'id':'estMsgResult'}).html('<h4>'+txts.res1+'</h4>'+msg).prependTo('#estMsgFormDiv');
+                        
+                        if(typeof ret.pvw !== 'undefined'){
+                          if(document.getElementById('estMsgPrevBelt')){$(ret.pvw[0]).prependTo('#estMsgPrevBelt');}
+                          else{$(ret.pvw[1]).appendTo('#estMsgPrevDiv');}
+                          }
+                        }
+                      },
+                    error: function(jqXHR, textStatus, errorThrown){
+                      console.log('ERRORS: '+textStatus+' '+errorThrown+' '+jqXHR.responseText);
+                      }
+                    });
+                  });
+                  
               }
             }).prependTo($('#estMsgSend1').parent());
-            
-          $('#estMsgSend1').fadeOut(200);
+          $('#estMsgSend0').closest('tr').hide();
+          $('#estMsgSend1').remove();
           $('#estMsgTerms').fadeOut(200);
           
         });
@@ -342,16 +397,23 @@ function htmlDecode(mode,xVal,dec=0){
     $('#estMsgSend1').prop('disabled',true);
     $('.estChkMsg').removeClass('estMsgNG');
     var mVal = Number($('select[name="msg_mode"]').find('option:selected').val());
+    //if(mVal == 3){$('#msgTopTR').show();}
+    //else{$('#msgTopTR').hide();}
     if(mVal > 0){
-      $('#estMsgFormTB').fadeIn(300);
       var sForm = 0;
+      $('#estMsgFormTB').fadeIn(300);
       $('.estChkMsg').each(function(i,elm){
         if($(elm).val().length < Number($(elm).data('len'))){$(elm).addClass('estMsgNG'); sForm++;}
         if(typeof $(elm).data('req') !== 'undefined' && $(elm).val().indexOf($(elm).data('req')) == -1){$(elm).addClass('estMsgNG'); sForm++;}
         }).promise().done(function(){
-          if(sForm == 0){
+          if(sForm > 0 && mode == 1){
+            var txts = $('#estMsgFormDiv').data('txts');
+            alert(txts.fng);
+            }
+          else{
             if(mode == 1){estMsgSnd();}
             else{
+              $('#estMsgSend0').closest('tr').hide();
               $('#estMsgSend1').prop('disabled',false).removeProp('disabled');
               $('#estMsgFormTF').fadeIn(300);
               }
@@ -368,19 +430,55 @@ function htmlDecode(mode,xVal,dec=0){
   
   
   function estMsgSys(){
+    if(document.getElementById('estMsgWarn')){return;}
+    if(document.getElementById('estMsgPrevDiv')){
+      $('#estMsgPrevDiv h4').on({
+        click :function(){
+          if($('#estMsgPrevBelt').is(':visible')){
+            $('#estMsgPrevBelt').animate({'height':'0px'},500,'swing',function(){$('#estMsgPrevBelt').hide()});
+            $('.estMsgP').hide();
+            }
+          else{$('#estMsgPrevBelt').show().animate({'height':'256px'},500,'swing');}
+          }
+        });
+      
+      $('.estMsgBtn').each(function(i,btn){
+        $(btn).on({
+          click : function(e){
+            e.preventDefault();
+            var targ = $(btn).find('div.estMsgP')
+            if($(targ).is(':visible')){$(targ).hide();}
+            else{
+              $('.estMsgP').hide();
+              $(targ).show();
+              }
+            }
+          })
+        });
+      }
+    
     if(document.getElementById('estMsgFormTabl')){
-      $('.estChkMsgRem').on({change : function(e){if(this.value !== 'istatrap@icloud.com'){$('#estMsgFormTabl').parent().remove();}}});
-      $('select[name="msg_mode"]').data('m',['',$('#estMsgDef1').html(),$('#estMsgDef2').html(),'']).on({
+      $('.estChkMsgRem').on({change : function(e){if(this.value !== 'itsatrap@'+$(this).data('domn')){$('#estMsgFormTabl').parent().remove();}}});
+      $('#estMsgFormDiv').data({'txts':{'fng':$('#estMsgFNG').html(),'res1':$('#estMsgRes').html(),'send1':$('#estEmS1').html(),'txt0':$('#estEmSent0').html(),'txt1':$('#estEmSent1').html(),'txt2':$('#estEmSent1').html(),'txt3':$('#estPmSent').html(),'txt4':$('#estEmSent4').html(),'txt5':$('#estEmSent5').html(),'txt6':$('#estThks1').html(),'txt7':$('#estThks2').html()}});
+      
+      $('select[name="msg_mode"]').data({'m':['',$('#estT1').html(),$('#estT2').html(),''],'t':['',$('#estS1').html(),$('#estS2').html(),'']}).on({
         change : function(e){
           $(this).find('option[value="0"]').remove();
-          $('textarea[name="msg_text"]').val($(this).data('m')[$(this).find('option:selected').val()]);
+          var ky = Number($(this).find('option:selected').val());
+          $('textarea[name="msg_text"]').val($(this).data('m')[ky]);
+          if($(this).data('t')[ky].length > 4){$('input[name="msg_top"]').val($(this).data('t')[ky]).prop('disabled',true);}
+          else{$('input[name="msg_top"]').prop('disabled',false).removeProp('disabled').val('');}
           estMsgChk();
+          },
+        click : function(){
+          $('#estMsgPrevBelt').css({'height':'0px'}).hide();
+          $('.estMsgP').hide();
           }
         });
       $('.estChkMsg').on({blur: function(){estMsgChk()}});
+      $('#estMsgSend0').on({click : function(){estMsgChk(1);}});
       $('#estMsgSend1').on({click : function(){estMsgChk(1);}});
-      $('#estMsgDef1').remove();
-      $('#estMsgDef2').remove();
+      $('#estMsgDefs').remove();
       }
     }
   
