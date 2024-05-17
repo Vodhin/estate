b545c10983b0958a78ffb6140fe17c19239e9705
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-05-17 14:51:26 -0400

    CSS and Template Updates - more coming

--- templates/estate_template.php
+++ templates/estate_template.php
@@ -1,261 +1,245 @@
 <?php
-// estate Template file
-
-// IMPORTANT: if making or changing this template, there are certain IDs starting with "estViewBox" (eg estViewBoxTop, estViewBoxSummary, etc)
-// that are used by the Layout Preferences for this plugin and must be retained for this template to work.
-
+/* Estate Plugin Template File 
+ * 
+ * To Modify: 
+ * $ESTATE_TEMPLATE['view']['mylayout']['tempname'] = 'My Layout View Template'; //used for Layout Select in Estate Preferences
+ * $ESTATE_TEMPLATE['view']['mylayout']['txt'][0] = '<div class="mySpecialClass">{SHORTCODE}</div>'; //['txt'][0] ordering is implemented in listing.php 
+ * 
+ * 
+ */
 if (!defined('e107_INIT')) { exit; }
+// Estate Template file:  default layout
 
-global $EST_PREF,$EST_SPACES,$EST_GALEXP,$IDIV;
+global $sc,$EST_PREF,$EST_PROP,$EST_SPACES,$IDIV;
+$ns = e107::getRender();
+$tp = e107::getParser();
 
-if(defined('THEME_LAYOUT')){
-  if($EST_PREF['layout_view_spaces'] !== 'dynamicf' && strpos(strtolower(THEME_LAYOUT),'full') === false){$EST_PREF['layout_view_spaces'] = 'default';}
-  }
+$EST_MENU1 = e107::getMenu()->isLoaded('estate');
 
-    
+
 $ESTATE_TEMPLATE = array();
-//CORE TEMPLATE
-$ESTATE_TEMPLATE['noview'] = '
-<div id="estateCont">
-  <h3>'.EST_GEN_PROPERTY.' #'.$GLOBALS['PROPID'].' '.EST_GEN_NOTFOUND.'</h3>
-  <div>'.EST_ERR_HLP1a.' '.$GLOBALS['PROPID'].' '.EST_ERR_HLP1b.' <a href="'.e_SELF.'"><b>'.EST_GEN_VIEWLISTINGS.'</b></a></div>
-</div>';
-
-$ESTATE_TEMPLATE['nolist'] = '
-<div id="estateCont">
-  <h3>'.EST_GEN_NOLISTINGS.'</h3><div>'.EST_GEN_CHECKLATER.'</div>
-</div>';
-
-$ESTATE_TEMPLATE['start'] = '
-<div id="estateCont">';
-
-$ESTATE_TEMPLATE['end'] = '
-</div>';
-
-$ESTATE_TEMPLATE['pins'] = '{PROP_MAP_PINS}';
 
 
-//LIST OF LISTINGS
-$ESTATE_TEMPLATE['list']['elnk'] = '<div id="estMiniSrc">{PROP_NEWICON}</div>';
 
-if(intval($EST_PREF['layout_list_map']) > 0){
-  $ESTATE_TEMPLATE['list']['map']	= '
-  <div id="estMapCont" class="'.$EST_PREF['layout_list_mapbg'].'">
-    <div id="estMap" style="width: 100%;"></div>
-  </div>';
-  }
-else{
-  $ESTATE_TEMPLATE['list']['map'] = '';
+//VIEW TEMPLATE
+if(!isset($EST_VIEW_SUMMARY)){
+  $EST_VIEW_SUMMARY = '
+      <div class="estViewSect">
+        <h4>{PROP_NAME}</h4>
+        {PROP_FEATURES}
+        <p>{PROP_SUMMARY}</p>
+        <p>{PROP_DESCRIPTION}</p>
+      </div>';
   }
 
 
-//{PROP_SUBDIVNAME}
-$ESTATE_TEMPLATE['list']['item'] 	= '
-  <a class="estListBlockA" href="{PROP_LIST_LINKVIEW}">
-    <div class="estListBlock '.$EST_PREF['layout_list'].'">
-      {PROP_EDITICONS:for=list}
-      <div class="estListPropHead">
-        <div class="estListPropHeadD1">{PROP_NAME}</div>
-        <div class="estListPropHeadD2">{PROP_BULLETS1} • {PROP_VIEWCOUNT}</div>
-        <div class="estListPropHeadD3">{PROP_CITYSTATE}</div>
-      </div>
-      <div class="estListPropImg" style="{PROP_THMSTY}">{PROP_LISTTHM_BANNER}</div>
-      <div class="estListPropInfo">
-        <div class="estILBullets">
-          <div class="estModlNam">{PROP_MODELNAME}</div>
-          {PROP_FEATURES}
-        </div>
-        <div class="estListSummary">
-          {PROP_SUMMARY}
-        </div>
-      </div>
-    </div>
-  </a>';
 
-
-
-$ESTATE_TEMPLATE['menu']['head'] = '<div id="estPlugMenu0Cap">{PROP_MENU_HEAD}</div>';
-
-$ESTATE_TEMPLATE['menu']['saved'] = '
-  <div id="estSavedModule" class="estViewSect noPADTB">
-    <h3>Saved Listings</h3>
-    {PROP_SAVED_LIST}
-  </div>
-  ';
-
-$ESTATE_TEMPLATE['menu']['seller'] = '
-    <div id="estViewBoxSummaryMenu">
-      <div id="estInfoModule" class="estViewSect">
-        <div class="estViewMod">
-          <h3 id="estSidebar1Capt" class="sumCapt">{PROP_STATUS}</h3>
-          <div><span class="FR">{PROP_VIEWCOUNT}</span>{PROP_PRICE}</div>
-          <div>{PROP_MODELNAME}</div>
-        </div>
-      </div>
-      
-      <div id="estAgntModule" class="estViewSect noPADTB">
-        {PROP_AGENTCARD:img=1}
-          {PROP_MSG_CARD}
-        </div>
-      </div>
-      
-      <div id="estOpenHouseModule" class="estViewSect">
-        <div class="estViewMod">
-          <h3 class="caption">'.EST_GEN_EVENTS.'</h3>
-          <div class="estDet2Cont">
-            {PROP_VIEW_OPENHOUSE}
+if(!isset($EST_VIEW_FEATURES)){
+  $EST_VIEW_FEATURES = '
+      <div class="estViewSect">
+        <div id="estInfoModule1">
+          <div class="estInfoCard">
+            {PROP_POINTS}
+          </div>
+          <div class="estInfoCard">
+            {PROP_FEATURE_EXTENDED}
           </div>
         </div>
-      </div>
-    </div>
-  ';
+      </div>';
+  }
 
 
-//VIEW LISTING TEMPLATE
-//{PROP_NEWLNK}{PROP_EDITLNK}
-$ESTATE_TEMPLATE['view']['elnk'] = '<div id="estMiniSrc">{PROP_NEWICON}{PROP_EDITICONS:for=view}</div>';
+  
 
-$ESTATE_TEMPLATE['view']['top'] = '
-  <div id="estViewBoxTop"'.($EST_PREF['slideshow_act'] == 1 ? ' class="estSlideshow" title="'.EST_GEN_PLAYPAUSESLIDE.'"' : '').'>
+if(!isset($EST_VIEW_SPACES_DYNAMIC)){
+  $EST_VIEW_SPACES_DYNAMIC = '
+        <div id="estViewBoxSpacesCont" class=" estSpaceDynamic'.($EST_PREF['slideshow_act'] == 1 ? ' estSlideshow' : '').'">
+          <div id="estViewSpacePvwCont">
+            {PROP_SPACEPVW}
+          </div>
+          <div id="estViewSpaceBtnCont">
+            <div id="estViewSpaceBtns" class="estViewSect">
+              {PROP_VIEWSPACEBTNS}
+            </div>
+          </div>
+        </div>';
+  }
+
+if(!isset($EST_VIEW_SPACES_TILES)){
+  $EST_VIEW_SPACES_TILES = '
+          <div id="estViewBoxSpacesCont" class=" '.($EST_PREF['slideshow_act'] == 1 ? ' estSlideshow' : '').' WD100">
+              <div id="estViewSpaceBtnCont">
+                {VIEW_SPACESTABLE}
+              </div>
+          </div>';
+  }
+
+
+  
+  
+  
+
+  
+
+if(!isset($EST_VIEW_NXPR)){
+  $EST_VIEW_NXPR = '
+        <div id="estViewNxtPre" class="estViewBoxBot WD100">
+          <div class="estViewSect">
+            {PROP_NPLISTING}
+          </div>
+        </div>';
+  }
+
+
+
+
+
+/*
+ * SIDE BAR ELEMENTS:
+ * used in estate_menu.php (if NOT full width) OR listings.php?view (if full width)
+ * desgned to be parsed and rentered via $ns->tablerender
+ *
+ */
+
+$ESTATE_TEMPLATE['menu']['title']['cap'] = '<div id="estMenuPropTop">{PROP_NAME}{PROP_MENU_HEAD}</div>';
+$ESTATE_TEMPLATE['menu']['title']['txt'] = '
+        <div class="estViewSect noPADTB">
+          <div class="estViewMod">
+            <h3 class="sumCapt">{PROP_STATUS}</h3>
+            <div><span class="FR">{PROP_VIEWCOUNT}</span>{PROP_PRICE}</div>
+            <div>{PROP_MODELNAME}</div>
+          </div>
+        </div>';
+
+
+$ESTATE_TEMPLATE['menu']['seller']['cap'] = '<div>{AGENT_ROLL}</div>';
+$ESTATE_TEMPLATE['menu']['seller']['txt'] = '<div class="estAgntModule noPADTB">{PROP_AGENTCARD:img=1}</div>';
+
+$ESTATE_TEMPLATE['menu']['events']['txt'] = '<div id="estEventList">{PROP_VIEW_OPENHOUSE}</div>';
+
+$ESTATE_TEMPLATE['menu']['spaces']['cap'] = '<div>'.EST_GEN_SPACES.'</div>';
+$ESTATE_TEMPLATE['menu']['spaces']['txt'] = '<div id="estSpacesMenu">{SPACES_MENU}</div>';
+
+
+$ESTATE_TEMPLATE['menu']['saved']['txt'] = '<div id="estSavedPropList">{PROP_SAVED_LIST}</div>';
+
+
+
+
+
+
+$ns->setStyle('menu');
+$ESTATE_TEMPLATE['view']['default']['tempname'] = 'Default View Template'; //used for Layout Select in Estate Preferences
+
+//SlideShow Section
+$ESTATE_TEMPLATE['view']['default']['txt'][0] = '
+  <div id="estSlideShow" class="'.($EST_PREF['slideshow_act'] == 1 ? ' estSlideshow" title="'.EST_GEN_PLAYPAUSESLIDE.'"' : '"').'>
     <div class="estSSPlayPause"></div>
   </div>';
 
 
-
-
-$ESTATE_TEMPLATE['view']['sum'] = '
-  <div id="estViewBoxSummary" class="estViewBoxMid '.$EST_PREF['layout_view_agent'].' flexStretch '.$EST_PREF['layout_view_summbg'].'">
-    <div id="estViewBoxSummarySB" class="'.$EST_PREF['layout_view_agntbg'].'">
-      <div class="estViewBoxCont">
-        <div id="estInfoModule" class="estViewSect">
-          <div class="estViewMod">
-            <h3 id="estSidebar1Capt" class="sumCapt">{PROP_STATUS}</h3>
-            <div><span class="FR">{PROP_VIEWCOUNT}</span>{PROP_PRICE}</div>
-            <div>{PROP_MODELNAME}</div>
-          </div>
-        </div>
-        <div id="estAgntModule" class="estViewSect noPADTB">
-          {PROP_AGENTCARD:img=1}
-        </div>
-        <div id="estOpenHouseModule" class="estViewSect">
-          <div class="estViewMod">
-            <h3 class="caption">'.EST_GEN_EVENTS.'</h3>
-            <div class="estDet2Cont">
-              {PROP_VIEW_OPENHOUSE}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="estViewBoxSummaryOverview">
-      <div class="estViewBoxCont">
-        <div class="estViewSect">
-          <h3 class="caption">'.EST_GEN_OVERVIEW.'</h3>
-          <h4>{PROP_NAME}</h4>
-          {PROP_FEATURES}
-          <p>{PROP_SUMMARY}</p>
-          <p>{PROP_DESCRIPTION}</p>
-        </div>
-        <div id="estFeaturesModule" class="estViewSect">
-          <h3 id="estFeaturesCaption" class="caption">'.EST_GEN_FEATURES.'</h3>
-          <div id="estInfoModule1">
-            <div class="estInfoCard">
-              {PROP_POINTS}
-            </div>
-            <div class="estInfoCard">
-              {PROP_FEATURE_EXTENDED}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>';
+// Summary Section
+$ESTATE_TEMPLATE['view']['default']['txt'][1] = '<div class="estFLEXCont flexRev flexStretch">';
+// include Summary Menu if Theme layout is 'full' and no est_menu.php loaded
+if($EST_MENU1 == 0 || strpos(strtolower(THEME_LAYOUT),'full') !== false){
+  $CAPT = $tp->parseTemplate($ESTATE_TEMPLATE['menu']['title']['cap'], false, $sc);
+  $TXT = $tp->parseTemplate($ESTATE_TEMPLATE['menu']['title']['txt'], false, $sc);
+  $TXT .= $tp->parseTemplate($ESTATE_TEMPLATE['menu']['seller']['txt'], false, $sc);
+  $TXT .= $tp->parseTemplate($ESTATE_TEMPLATE['menu']['events']['txt'], false, $sc);
   
+  $ESTATE_TEMPLATE['view']['default']['txt'][1] .= '<div class="estSummaryMenu">'.$ns->tablerender($CAPT,$TXT,'menu',true).'</div>';
+  unset($CAPT,$TXT);
+  define("ESTAGENTRENDERED",1);
+  }
 
-//
-$EST_PREF['layout_view_spaces'] = 'tiles';
+$ESTATE_TEMPLATE['view']['default']['txt'][1] .= '<div class="estSummaryMain"><div class="WD100">';
+$TXT = $tp->parseTemplate($EST_VIEW_SUMMARY, false, $sc);
+$ESTATE_TEMPLATE['view']['default']['txt'][1] .= $ns->tablerender(EST_GEN_OVERVIEW,$TXT,'menu',true);
+$TXT = $tp->parseTemplate($EST_VIEW_FEATURES, false, $sc);
+$ESTATE_TEMPLATE['view']['default']['txt'][1] .= $ns->tablerender(EST_GEN_FEATURES,$TXT,'menu',true);
+$ESTATE_TEMPLATE['view']['default']['txt'][1] .= '</div></div></div>';
 
-if(count($GLOBALS['EST_SPACES'])){
-  $ESTATE_TEMPLATE['view']['spaces'] = '
-  <div id="estViewBoxSpacesCont" class="'.$EST_PREF['layout_view_spacesbg'].($EST_PREF['layout_view_spaces'] == 'dynamic' ? ' estSpaceDynamic' : '').($EST_PREF['layout_view_spaces_ss'] == 1 ? ' estSlideshow' : '').'">';
-  
-  if($EST_PREF['layout_view_spaces'] == 'dynamic'){
-    $ESTATE_TEMPLATE['view']['spaces'] .= '<div class="estViewBoxMid">
-      <div id="estViewSpacePvwCont">
-        <div class="estViewBoxCont">
-          <h3 id="estViewBoxSpacesCapt" class="caption">'.EST_GEN_SPACES.'</h3>
-          <div id="estViewSpaceImgPvwCont" class="estBGGrad1">
-            <div id="estViewSpaceImgPvwSlider" class="estViewSect">
-              <div id="estArrBordR"></div>
-              <div id="estViewSpaceImgCont">{PROP_VIEWSPACEIMGS}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="estViewSpaceBtnCont">
-        <div class="estViewBoxCont">
-          <div id="estViewSpaceBtns" class="estViewSect">{PROP_VIEWSPACEBTNS}</div>
-        </div>
-      </div>
-    </div>';
-    }
-  else{
-    $ESTATE_TEMPLATE['view']['spaces'] .= '<div class="estViewBoxMid">
-      <div id="estViewSpaceBtnCont">
-        <div class="estViewBoxCont">
-          <div id="estViewSpaceBtns" class="estViewSect">{PROP_VIEWSPACEBTNS}</div>
-        </div>
-      </div>
-    </div>';
-    }
-  $ESTATE_TEMPLATE['view']['spaces'] .= '
-  </div>';
+
+
+if(strpos(strtolower(THEME_LAYOUT),'full') > -1){
+  $TXT = $tp->parseTemplate($EST_VIEW_SPACES_DYNAMIC, false, $sc);
+  //$ESTATE_TEMPLATE['view']['default'][0] .= $ns->tablerender(EST_GEN_SPACES,$TXT,'menu',true);
+  $ESTATE_TEMPLATE['view']['default']['txt'][2] .= $tp->parseTemplate($EST_VIEW_SPACES_TILES, false, $sc);
   }
 else{
-  $ESTATE_TEMPLATE['view']['spaces'] = '';
+  $ESTATE_TEMPLATE['view']['default']['txt'][2] .= $tp->parseTemplate($EST_VIEW_SPACES_TILES, false, $sc);
   }
 
 
+$ESTATE_TEMPLATE['view']['default']['txt'][3] .= '<div class="WD100">'.$ns->tablerender(EST_GEN_MAP,'{EST_LEAFLET_MAP}','menu',true).'</div>';
 
-if(intval($EST_PREF['layout_view_map']) > 0){
-  $ESTATE_TEMPLATE['view']['map'] = '
-  <div id="estViewBoxMap" class="estViewBoxBot '.$EST_PREF['layout_view_mapbg'].'">
-    <div class="estViewBoxCont">
-      <div class="estViewSect">
-        <h3 class="caption">'.EST_GEN_MAP.'</h3>
-        <div id="estMapCont" data-zm="'.$EST_PREF['map_zoom_def'].'">
-          <div id="estMap"></div>
-        </div>
-      </div>
+
+//$ESTATE_TEMPLATE['view']['default'][0] .= $tp->parseTemplate($EST_VIEW_NXPR, false, $sc);
+
+$ESTATE_TEMPLATE['view']['default']['txt'][4] .= '<div class="WD100">'.$ns->tablerender(EST_GEN_IMAGE.' '.EST_GEN_GALLERY,'{EST_VIEW_GALLERY:imgs='.$IDIV.'}','menu',true).'</div>';
+
+$ESTATE_TEMPLATE['view']['default']['txt'][5] .= '<div id="estMiniSrc">{PROP_NEWICON}{PROP_EDITICONS:for=view}</div>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$EST_LIST_CAP = '
+<div class="estCardTop">
+  {PROP_LIST_BANNER}
+  {PROP_EDITICONS:for=card}
+  <a href="{PROP_LIST_LINKVIEW}">
+    <div class="estCardPropName">{PROP_NAME}</div>
+    <div class="estCardPropCity">{PROP_CITYSTATE}</div>
+    <div class="estCardPropBul1">{PROP_BULLETS1} • {PROP_VIEWCOUNT}</div>
+  </a>
+</div>';
+
+$EST_LIST_TXT = '
+<div class="estCardMain">
+  <a href="{PROP_LIST_LINKVIEW}">
+    <div class="estListCardImg" style="{PROP_THMSTY}"></div>
+    <div class="estILBullets">
+      <div class="estModlNam">{PROP_MODELNAME}</div>
+      {PROP_FEATURES}
+      {PROP_SUMMARY}
     </div>
-  </div>';
+  </a>
+  {PROP_AGENTMINI:for=card}
+</div>';
+
+
+
+
+$ESTATE_TEMPLATE['list']['default']['tempname'] = 'Default List Template';
+$ESTATE_TEMPLATE['list']['default']['txt'] = '{EST_LEAFLET_MAP}';
+
+$ESTATE_TEMPLATE['list']['default']['txt'] .= '<div class="estFLEXCont">';
+
+if(count($EST_PROP) > 0){
+  $slc = e107::getScBatch('estate',true);
+  foreach($EST_PROP as $k=>$v){
+    $slc->setVars($v);
+    $CAPT = $tp->parseTemplate($EST_LIST_CAP, false, $slc);
+    $TXT = $tp->parseTemplate($EST_LIST_TXT, false, $slc);
+    $ESTATE_TEMPLATE['list']['default']['txt'] .= '<div class="card estCard">'.$ns->tablerender($CAPT,$TXT,'menu',true).'</div>';
+    unset($CAPT,$TXT);
+    }
   }
-else{
-  $ESTATE_TEMPLATE['view']['map'] = '';
-  }
 
-
-$ESTATE_TEMPLATE['view']['nplisting']	= '
-  <div id="estViewNxtPre" class="estViewBoxBot '.$EST_PREF['layout_view_gallbg'].'">
-    <div class="estViewBoxCont">
-      <div class="estViewSect">
-        {PROP_NPLISTING}
-      </div>
-    </div>
-  </div>
-  ';
+$ESTATE_TEMPLATE['list']['default']['txt'] .= '</div>';
+  
+$ESTATE_TEMPLATE['list']['default']['txt'] .= '<div id="estMiniSrc">{PROP_NEWICON}</div>';
 
 
 
-$ESTATE_TEMPLATE['view']['gal'] = '
-  <div id="estViewBoxGallery" class="estViewBoxBot '.$EST_PREF['layout_view_gallbg'].'">
-    <div class="estViewBoxCont">
-      <div id="estGalSect" class="estViewSect">
-        <h3 id="estGalContH2" class="caption">'.EST_GEN_IMAGE.' '.EST_GEN_GALLERY.'</h3>
-        <div id="estGalCont" {PROP_VIEWGALSTYLE}>'.$IDIV.'</div>
-      </div>
-    </div>
-  </div>';
-
- //{SETIMAGE: w=400&h=300} {PROP_OPENHOUSELI}
-
+unset($EST_LIST_CAP,$EST_LIST_TXT);
