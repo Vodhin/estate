9bd76b2b656be77efa2e960dc217d8fac2295d48
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-07-11 15:35:13 -0400

    Updated Uninstall Functions

--- ui/preview.php
+++ ui/preview.php
@@ -1,64 +1,64 @@
-<?php
-if(!defined('e107_INIT')) { exit; }
-if(!ADMIN){exit;}
-if(!$qs){exit;}
-
-$e107_popup = 1;
-e107::css('inline', 'body{padding-top: 0px;}');
-
-if(strpos($qs[3],'|') > -1){
-  $NP = explode('|',$qs[3]);
-  
-  }
-
-
-if($PROPID > 0){
-  $ESTDTA = estGetSpaces($PROPID);
-  $IDIV = estViewCSS($ESTDTA);
-  $EST_SPACES = $ESTDTA[1];
-  $PROPDTA[0] = $EST_PROP[$PROPID];
-  
-  $PINS = est_map_pins();
-  e107::js('inline','var estMapPins = '.$PINS.'; ', 'jquery',2);
-  
-  require_once(HEADERF);
-  $tmpl = e107::getTemplate('estate');
-  $sc = e107::getScBatch('estate',true);
-  $sc->setVars($PROPDTA[0]);
-  
-  if(strpos(strtolower(THEME_LAYOUT),'full') === false){
-    $EST_PREF['layout_view_spaces'] = 'tiles';
-    $tmpl = e107::getTemplate('estate');
-    $sc = e107::getScBatch('estate',true);
-    }
-  
-  $estText .= $tp->parseTemplate($tmpl['view'][$qs[2]], false, $sc);
-  $ns->tablerender('','<div id="estDtaCont" class="estPrefScale75">'.$estText.'</div>','estate-view'); //
-  }
-else{
-  $PINS = est_map_pins();
-  e107::js('inline','var estMapPins = '.$PINS.'; ', 'jquery',2);
-  require_once(HEADERF);
-  $tmpl = e107::getTemplate('estate');
-  $sc = e107::getScBatch('estate',true);
-  $sc->setVars($PROPDTA[0]);
-  
-  $estText = $tp->parseTemplate($tmpl['list']['top'], false, $sc);
-  if(count($EST_PROP) > 0){
-    foreach($EST_PROP as $k=>$v){
-      $sc->setVars($v);
-      $estText .= $tp->parseTemplate($tmpl['list']['item'], false, $sc);
-      }
-    }
-  $estText .= $tp->parseTemplate($tmpl['list']['bot'], false, $sc);
-  
-  $ns->tablerender('','<div id="estDtaCont" class="estPrefScale75">'.$estText.'</div>','estate-list');
-  unset($estText);
-  }
-
-echo '
-<div id="estJSpth"'.$OACLASS.' data-pth="'.EST_PATHABS.'"></div>
-<div id="estMobTst"></div>';
-require_once(FOOTERF);
-
+<?php
+if(!defined('e107_INIT')) { exit; }
+if(!ADMIN){exit;}
+if(!$qs){exit;}
+
+$e107_popup = 1;
+e107::css('inline', 'body{padding-top: 0px;}');
+
+if(strpos($qs[3],'|') > -1){
+  $NP = explode('|',$qs[3]);
+  
+  }
+
+
+if($PROPID > 0){
+  $ESTDTA = estGetSpaces($PROPID);
+  $IDIV = estViewCSS($ESTDTA);
+  $EST_SPACES = $ESTDTA[1];
+  $PROPDTA[0] = $EST_PROP[$PROPID];
+  
+  $PINS = est_map_pins();
+  e107::js('inline','var estMapPins = '.$PINS.'; ', 'jquery',2);
+  
+  require_once(HEADERF);
+  $tmpl = e107::getTemplate('estate');
+  $sc = e107::getScBatch('estate',true);
+  $sc->setVars($PROPDTA[0]);
+  
+  if(strpos(strtolower(THEME_LAYOUT),'full') === false){
+    $EST_PREF['layout_view_spaces'] = 'tiles';
+    $tmpl = e107::getTemplate('estate');
+    $sc = e107::getScBatch('estate',true);
+    }
+  
+  $estText .= $tp->parseTemplate($tmpl['view'][$qs[2]], false, $sc);
+  $ns->tablerender('','<div id="estDtaCont" class="estPrefScale75">'.$estText.'</div>','estate-view'); //
+  }
+else{
+  $PINS = est_map_pins();
+  e107::js('inline','var estMapPins = '.$PINS.'; ', 'jquery',2);
+  require_once(HEADERF);
+  $tmpl = e107::getTemplate('estate');
+  $sc = e107::getScBatch('estate',true);
+  $sc->setVars($PROPDTA[0]);
+  
+  $estText = $tp->parseTemplate($tmpl['list']['top'], false, $sc);
+  if(count($EST_PROP) > 0){
+    foreach($EST_PROP as $k=>$v){
+      $sc->setVars($v);
+      $estText .= $tp->parseTemplate($tmpl['list']['item'], false, $sc);
+      }
+    }
+  $estText .= $tp->parseTemplate($tmpl['list']['bot'], false, $sc);
+  
+  $ns->tablerender('','<div id="estDtaCont" class="estPrefScale75">'.$estText.'</div>','estate-list');
+  unset($estText);
+  }
+
+echo '
+<div id="estJSpth"'.$OACLASS.' data-pth="'.EST_PATHABS.'"></div>
+<div id="estMobTst"></div>';
+require_once(FOOTERF);
+
 ?>
--- ui/qry.php
+++ ui/qry.php
@@ -1,54 +1,54 @@
-<?php
-if (!defined('e107_INIT')) { exit; }
-
-$EST_PROP = array();
-$EST_PROPINTS = array('prop_idx','prop_status','prop_type','prop_zoning','prop_agent','prop_leasedur','prop_leasefreq','prop_landfee','prop_landfreq','prop_hoafee','prop_hoareq','prop_hoafrq','prop_yearbuilt','prop_intsize','prop_landsize','prop_bathhalf','prop_bathtot','prop_bedtot','prop_bathfull','prop_bathmain','prop_bedmain','prop_floorct','prop_floorno','prop_bldguc','prop_complxuc','prop_views');
-
-foreach($estQdta as $row){
-  foreach($row as $k=>$v){
-    if(in_array($k,$EST_PROPINTS)){$row[$k] = intval($v);}
-    //prop_appr
-    }
-  
-  if($prop_idx !== intval($row['prop_idx'])){
-    $prop_idx = intval($row['prop_idx']);
-    $EST_PROP[$prop_idx] = $row;
-    $FPROPS .= ($FPROPS ? "," : "").$prop_idx;
-    }
-  }
-
-
-
-if($data2 = $sql->retrieve("SELECT #estate_subdiv.* FROM #estate_subdiv ORDER BY subd_idx ASC",true)){
-  foreach($data2 as $row){
-    $EST_SUBDIV[$row['subd_idx']]['subd_city'] = $row['subd_city'];
-  	$EST_SUBDIV[$row['subd_idx']]['subd_name'] = $row['subd_name'];
-  	$EST_SUBDIV[$row['subd_idx']]['subd_url'] = $row['subd_url'];
-  	$EST_SUBDIV[$row['subd_idx']]['subd_hoaname'] = $row['subd_hoaname'];
-  	$EST_SUBDIV[$row['subd_idx']]['subd_hoaweb'] = $row['subd_hoaweb'];
-  	$EST_SUBDIV[$row['subd_idx']]['subd_hoareq'] = intval($row['subd_hoareq']);
-  	$EST_SUBDIV[$row['subd_idx']]['subd_hoafee'] = intval($row['subd_hoafee']);
-  	$EST_SUBDIV[$row['subd_idx']]['subd_hoafrq'] = intval($row['subd_hoafrq']);
-    }
-  }
-
-
-
-if(count($EST_PROP) > 0){
-  $EVTQRY = "SELECT #estate_events.* FROM #estate_events WHERE event_propidx";
-  if($PROPID == 0){
-    $EVTQRY .= " IN($FPROPS)";
-    if($data2 = $sql->retrieve("SELECT #estate_media.* FROM #estate_media WHERE media_propidx IN($FPROPS) AND media_galord ='1'",true)){
-      $i = 0;
-      foreach($data2 as $row){$EST_PROP[$row['media_propidx']]['img'][$row['media_galord']]['t'] = $row['media_thm'];}
-      }
-    }
-  else{$EVTQRY .= "='$PROPID'";}
-  $EVTQRY .= " AND event_start >='".$STRDATETODAY."' AND event_type='1' ORDER BY event_start ASC"; //AND event_stat='1' 
-  if($dataEvt = $sql->retrieve($EVTQRY,true)){
-    foreach($dataEvt as $row){$EST_PROP[$row['event_propidx']]['evt'][$row['event_start']] = $row;}
-    }
-  }
-
-unset($FPROPS,$EVTQRY,$MSGQRY,$data,$data2,$dataEvt,$dataMsg,$Z1,$Z2,$row);
+<?php
+if (!defined('e107_INIT')) { exit; }
+
+$EST_PROP = array();
+$EST_PROPINTS = array('prop_idx','prop_status','prop_type','prop_zoning','prop_agent','prop_leasedur','prop_leasefreq','prop_landfee','prop_landfreq','prop_hoafee','prop_hoareq','prop_hoafrq','prop_yearbuilt','prop_intsize','prop_landsize','prop_bathhalf','prop_bathtot','prop_bedtot','prop_bathfull','prop_bathmain','prop_bedmain','prop_floorct','prop_floorno','prop_bldguc','prop_complxuc','prop_views');
+
+foreach($estQdta as $row){
+  foreach($row as $k=>$v){
+    if(in_array($k,$EST_PROPINTS)){$row[$k] = intval($v);}
+    //prop_appr
+    }
+  
+  if($prop_idx !== intval($row['prop_idx'])){
+    $prop_idx = intval($row['prop_idx']);
+    $EST_PROP[$prop_idx] = $row;
+    $FPROPS .= ($FPROPS ? "," : "").$prop_idx;
+    }
+  }
+
+
+
+if($data2 = $sql->retrieve("SELECT #estate_subdiv.* FROM #estate_subdiv ORDER BY subd_idx ASC",true)){
+  foreach($data2 as $row){
+    $EST_SUBDIV[$row['subd_idx']]['subd_city'] = $row['subd_city'];
+  	$EST_SUBDIV[$row['subd_idx']]['subd_name'] = $row['subd_name'];
+  	$EST_SUBDIV[$row['subd_idx']]['subd_url'] = $row['subd_url'];
+  	$EST_SUBDIV[$row['subd_idx']]['subd_hoaname'] = $row['subd_hoaname'];
+  	$EST_SUBDIV[$row['subd_idx']]['subd_hoaweb'] = $row['subd_hoaweb'];
+  	$EST_SUBDIV[$row['subd_idx']]['subd_hoareq'] = intval($row['subd_hoareq']);
+  	$EST_SUBDIV[$row['subd_idx']]['subd_hoafee'] = intval($row['subd_hoafee']);
+  	$EST_SUBDIV[$row['subd_idx']]['subd_hoafrq'] = intval($row['subd_hoafrq']);
+    }
+  }
+
+
+
+if(count($EST_PROP) > 0){
+  $EVTQRY = "SELECT #estate_events.* FROM #estate_events WHERE event_propidx";
+  if($PROPID == 0){
+    $EVTQRY .= " IN($FPROPS)";
+    if($data2 = $sql->retrieve("SELECT #estate_media.* FROM #estate_media WHERE media_propidx IN($FPROPS) AND media_galord ='1'",true)){
+      $i = 0;
+      foreach($data2 as $row){$EST_PROP[$row['media_propidx']]['img'][$row['media_galord']]['t'] = $row['media_thm'];}
+      }
+    }
+  else{$EVTQRY .= "='$PROPID'";}
+  $EVTQRY .= " AND event_start >='".$STRDATETODAY."' AND event_type='1' ORDER BY event_start ASC"; //AND event_stat='1' 
+  if($dataEvt = $sql->retrieve($EVTQRY,true)){
+    foreach($dataEvt as $row){$EST_PROP[$row['event_propidx']]['evt'][$row['event_start']] = $row;}
+    }
+  }
+
+unset($FPROPS,$EVTQRY,$MSGQRY,$data,$data2,$dataEvt,$dataMsg,$Z1,$Z2,$row);
 ?>
