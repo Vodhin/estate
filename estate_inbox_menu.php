e8a30a6521e69275d5eeb03c577ad6155b6da2bf
Vodhin <135135923+Vodhin@users.noreply.github.com>
2024-11-29 10:54:11 -0500

    Fixed bug when not on an estate page

--- estate_inbox_menu.php
+++ estate_inbox_menu.php
@@ -7,11 +7,14 @@ if (!defined('e107_INIT')) { exit; }
 if (!e107::isInstalled('estate')) { return; }
 
 if(!defined("EST_RENDERED_INBOX")){
-  define("EST_RENDERED_INBOX",true);
-  if(EST_USERPERM > 0 || check_class(e107::pref('estate','contact_class'))){
-    e107::css('url',e_PLUGIN.'estate/css/msg.css');
-    e107::getRender()->setStyle('menu');
-    echo estMsgInbox();
+  if(function_exists('estMsgInbox')){
+    if(!defined('EST_MSGTYPES')){require_once(e_PLUGIN.'estate/estate_defs.php');}
+    define("EST_RENDERED_INBOX",true);
+    if(EST_USERPERM > 0 || check_class(e107::pref('estate','contact_class'))){
+      e107::css('url',e_PLUGIN.'estate/css/msg.css');
+      e107::getRender()->setStyle('menu');
+      echo estMsgInbox();
+      }
     }
   }
 ?>
